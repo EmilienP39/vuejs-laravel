82019bbe50f307299c85d0d1adbb41adc8730724
EmilienP39 <emilienpaul@hotmail.fr>
2022-04-11 11:59:08 +0200

    Debut donnees brut

--- intervention-vue/src/components/CRM.vue
+++ intervention-vue/src/components/CRM.vue
@@ -1,7 +1,29 @@
 <template>
   <h1>CRM</h1>
+  <div id="donnees">
+
+  </div>
 </template>
 
 <script>
+const donnees = [
+  {
+    nom:"toufick",
+    adresse: "dtc",
+    cp: "58000"
+  },{
+    nom:"maman toufick",
+    adresse: "dtc mais à côté",
+    cp: "58000"
+  },
+  {
+    nom:"papa toufick",
+    adresse: "dtc mais à côté",
+    cp: "58000"
+  }
+]
 
+for(let i; i<donnees.length;i++){
+  
+}
 </script>
